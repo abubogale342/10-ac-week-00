6c4edab05ed94bef8668d67c165f170b65131650
Habtamu Bogale <abubogale342@gmail.com>
2023-11-27 21:46:15 +0300

    loaded users and channels data

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,11 @@
+from loader import SlackDataLoader
+
 print("mondays assignment work")
 print("-----------------------------")
 
+slack_data = SlackDataLoader('./data/anonymized')
 
+users = slack_data.get_users()
+channels = slack_data.get_channels()
+
+print(channels)
